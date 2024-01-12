97833aa09c585edeff73b01a067800f9e1a52ebd
Vedant-07 <vedantnpatel7@gmail.com>
2024-01-13 02:51:12 +0530

    removed error due to config

--- trial7/static/vite1/src/App.jsx
+++ trial7/static/vite1/src/App.jsx
@@ -4,6 +4,8 @@ const App = () => {
     <>
   <div>App here to disaply ,finally ,a new adv. begins ,made some changes again !!!</div>
   <div>some words here again!!!!</div>
+  <h1>again here!!!</h1>
+  <div>made changes in vite.config</div>
    </>);
 };
 export default App;
